0a1ef8115e7df89cb1e33644ad4dd86b8afe7cf6
jypara-git <zhyparaabdieva@gmail.com>
2021-02-27 23:19:11 -0800

    add countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,49 @@ var questions = [
         correct: "It uses === and !== instead" 
     }
 ];
+const startingMinutes = 2;
+let time = startingMinutes * 60;
+let finalTime = 0;
+
+const countDownEl = document.getElementById("countdown");
+
+function updateCountdown() {
+    if (currentQuestionIndex >= questions.length) {
+        finalTime = time;
+        clearInterval(time = 0);
+    }
+    
+    if (time <= 0) {
+        var sectionContainer = document.querySelector("#container");
+        if (sectionContainer) {
+            sectionContainer.remove();
+        }
+       
+        clearInterval(time = 0);
+    }
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 2 ? '0' + seconds : seconds;
+    countDownEl.innerHTML = "Time-" + minutes + ":" +seconds;
+    time--;
+}
+var buttonElStart = document.querySelector("#start-button");
+
+
+var startQuiz = function(event) {
+    createQuestionContainer();
+    setInterval(updateCountdown, 1000);
+    var divStart = document.querySelector("#first-page");
+    var sectionQuestions = document.querySelector("#container");
+   
+ 
+
+};
+buttonElStart.addEventListener("click", startQuiz);
+
 var questionsFormEl = document.querySelector("#container");
 var currentQuestionIndex = 0;
+
 var handleSelectedButton = function(event) {
     var targetEl = event.target;
     console.log(targetEl.textContent);
@@ -55,10 +96,18 @@ var handleSelectedButton = function(event) {
         updateQuestionEL();
     }
     else {
-        console.log(false);
+        time = time - 15;
     }
-}
+};
+
 var updateQuestionEL = function() {
+    // all right answers
+    if (currentQuestionIndex >= questions.length) {
+        var sectionContainer = document.querySelector("#container");
+        sectionContainer.remove();
+
+        return;
+    }
     var questionDataObj = questions[currentQuestionIndex];
     var nameText = document.querySelector("#name-text");
     nameText.innerText = questionDataObj.name;
@@ -78,8 +127,53 @@ var updateQuestionEL = function() {
     buttonEl3.addEventListener("click", handleSelectedButton);
 
     buttonEl4.addEventListener("click", handleSelectedButton);
+
+};
+
+var createQuestionContainer = function() {
+    var startingPage = document.querySelector("#first-page");
+    startingPage.remove();
+
+    var sectionContainer = document.createElement("div");
+    sectionContainer.className = "outside-box";
+    sectionContainer.id = "container";
+
+    var nameText = document.createElement("h3");
+    nameText.className = "name-text";
+    nameText.id = "name-text";
+    sectionContainer.appendChild(nameText);
+
+    var buttonWrap = document.createElement("div");
+    buttonWrap.className = "btn-wrap";
+    
+    var buttonEl1 = document.createElement("button");
+    buttonEl1.className = "btn";
+    buttonEl1.id = "btn1";
+    buttonWrap.appendChild(buttonEl1);
+
+    var buttonEl2 = document.createElement("button");
+    buttonEl2.className = "btn";
+    buttonEl2.id = "btn2";
+    buttonWrap.appendChild(buttonEl2);
+
+    var buttonEl3 = document.createElement("button");
+    buttonEl3.className = "btn";
+    buttonEl3.id = "btn3";
+    buttonWrap.appendChild(buttonEl3);
+
+    var buttonEl4 = document.createElement("button");
+    buttonEl4.className = "btn";
+    buttonEl4.id = "btn4";
+    buttonWrap.appendChild(buttonEl4);
+
+    sectionContainer.appendChild(buttonWrap);
+    document.body.appendChild(sectionContainer);
+    updateQuestionEL();
+};
+var gameOverContainer = function() {
+    var allDone = document.createElement("div");
+    allDone.className = "outside-box";
+
+    var 
+    
 }
-
-
-
-updateQuestionEL();
